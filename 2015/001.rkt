69c4b6874d73084126dd6ae327ce3f4115e9b2dc
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2015-12-20 20:33:02 +0100

    repo reorg

--- 2015/001.rkt
+++ 2015/001.rkt
@@ -2,8 +2,10 @@
 
 (require srfi/41)
 
+; Part 1
 (for/sum ([p (in-port read-char)]) (if (char=? p #\u028) 1 -1))
 
+; Part 2
 (stream-length
   (stream-take-while (λ (i) (> i -1)) 
                      (stream-scan + 0 
