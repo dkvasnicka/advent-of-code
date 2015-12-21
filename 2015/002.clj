66d4a725f2c327053240037d97da57982cfae323
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2015-12-21 20:52:13 +0100

    Day 2, part 2

--- 2015/002.clj
+++ 2015/002.clj
@@ -2,15 +2,21 @@
   (mapv #(Integer/parseInt %)
         (clojure.string/split l #"x")))
 
+(defn double-and-sum [s]
+  (reduce + (map (partial * 2) s)))
+
 (defn wrap-present [[l w h]]
   (let [s1 (* l w)
         s2 (* w h)
         s3 (* h l)]
-    (+ (min s1 s2 s3)
-       (reduce + (map (partial * 2) [s1 s2 s3])))))
+    [(+ (min s1 s2 s3)
+        (double-and-sum [s1 s2 s3]))
+     (+ (* l w h)
+        (double-and-sum (take 2 (sort [l w h]))))]))
 
 (def xf (comp (map parse-line)
               (map wrap-present)))
 
 (println
-  (transduce xf + (line-seq (clojure.java.io/reader *in*))))
+  (transduce xf (partial map +) [0 0] 
+             (line-seq (clojure.java.io/reader *in*))))
