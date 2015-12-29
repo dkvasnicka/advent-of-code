469be759b7b71cea216ead7ca6316b48a47d9767
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2015-12-29 22:09:06 +0100

    Day 3, part 2

--- 2015/003.rkt
+++ 2015/003.rkt
@@ -7,6 +7,7 @@
     [#\v '(0 -1)]
     [#\< '(-1 0)]))
 
+; Part 1
 (define (aggregate)
   (for/fold ([s (set '(0 0))]
              [cur '(0 0)])
@@ -17,5 +18,17 @@
         (set-add s new-pos)
         new-pos))))
 
-(let-values ([(houses _) (aggregate)])
+; Part 2
+(define (aggregate-alternated)
+  (for/fold ([s (set '(0 0))]
+             [cur (hasheq 'santa '(0 0) 'robo '(0 0))])
+            ([dir (in-port read-char)]
+             [player (in-cycle '(santa robo))])
+
+    (let ([new-pos (map + (hash-ref cur player) (translate-directions dir))])
+      (values
+        (set-add s new-pos)
+        (hash-set cur player new-pos)))))
+
+(let-values ([(houses _) (aggregate-alternated)])
   (set-count houses))
