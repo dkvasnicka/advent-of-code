a84a56c9b29b13a132745a98ce6a4f18539bfa67
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2016-02-19 16:03:19 +0100

    Day 9, part 1

---
+++ 2015/009.rkt
@@ -0,0 +1,31 @@
+#lang racket
+
+(require graph)
+
+(define (lines-seq)
+  (sequence-map (curry regexp-match* #rx"(?![to])[a-zA-Z0-9]+")
+                (in-lines)))
+
+(define (read-edges)
+  (let ([g (weighted-graph/undirected '())])
+    (for ([l (lines-seq)])
+      (let ([weight (string->number (third l))])
+        (add-edge! g (first l) (second l) weight)
+        (add-edge! g (second l) (first l) weight))) 
+    g))
+
+(define (shortest-from g vertices start)
+  (let ([new-vset (set-remove vertices start)])
+    (cond
+      [(set-empty? new-vset) 0]
+      [(= (set-count vertices) (set-count new-vset)) +inf.0]
+      [else (sequence-fold min +inf.0
+              (sequence-map (λ (v) (+ (edge-weight g start v)
+                                      (shortest-from g new-vset v)))
+                            (in-neighbors g start)))])))
+
+(let* ([g (read-edges)]
+       [vertices (list->set (get-vertices g))])
+  (sequence-fold min +inf.0 
+                 (sequence-map (curry shortest-from g vertices) 
+                               (in-vertices g))))
