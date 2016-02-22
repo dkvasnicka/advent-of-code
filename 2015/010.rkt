39578ae3882b98c2c27f217527514ddef13934e9
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2016-02-22 12:45:08 +0100

    Day 10, part 1

---
+++ 2015/010.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+(require data/collection)
+
+(define (counting-drop-while fn? strm (cnt 0))
+  (if (or (empty? strm) (not (fn? (first strm))))
+    (values cnt strm)
+    (counting-drop-while fn? (rest strm) (add1 cnt))))
+
+(define (look-and-say strm)
+  (if (empty? strm)
+    '()
+    (let*-values ([(head) (first strm)]
+                  [(cnt tail) (counting-drop-while (curry = head) strm)])
+      (stream-cons cnt
+                   (stream-cons head
+                                (look-and-say tail))))))
+
+(length
+  (for/fold ([initval '(1 1 1 3 1 2 2 1 1 3)])
+            ([i 40])
+    (look-and-say initval)))
