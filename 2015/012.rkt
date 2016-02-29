06e626d88662c25da72d2820ff73dedde537bbc8
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2016-02-29 21:40:41 +0100

    Day 12, part 2

--- 2015/012.rkt
+++ 2015/012.rkt
@@ -7,7 +7,10 @@
     (cond
       [(number? e) e]
       [(list? e) (sum-all e)]
-      [(hash? e) (sum-all (in-hash-values e))]
+      [(hash? e) (let ([vals (in-hash-values e)]) 
+                   (if (sequence-ormap (curry equal? "red") vals) 
+                     0 
+                     (sum-all vals)))]
       [else 0])))
 
 (sum-all (read-json))
