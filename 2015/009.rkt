3d6e1bd43d3ca05bf02717ace414a544d56e4c49
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2016-03-07 22:58:06 +0100

    Day 13 part 1 - day 9 impl. changed to do travelling salesman problem, needs refactoring

--- 2015/009.rkt
+++ 2015/009.rkt
@@ -2,6 +2,8 @@
 
 (require graph)
 
+(provide find-path)
+
 (define (lines-seq)
   (sequence-map (curry regexp-match* #rx"(?![to])[a-zA-Z0-9]+")
                 (in-lines)))
@@ -12,20 +14,22 @@
       (add-edge! g (first l) (second l) (string->number (third l))))
     g))
 
-(define (shortest-from g vertices start)
+(define (shortest-from g vertices origin start)
   (let ([new-vset (set-remove vertices start)])
     (cond
-      [(set-empty? new-vset) 0]
+      [(set-empty? new-vset) (edge-weight g start origin)]
       [(= (set-count vertices) (set-count new-vset)) -inf.0]
       [else (sequence-fold max -inf.0
               (sequence-map (λ (v) (+ (edge-weight g start v)
-                                      (shortest-from g new-vset v)))
+                                      (shortest-from g new-vset origin v)))
                             (in-neighbors g start)))])))
 
-(let* ([g (read-edges)]
-       [vertices (list->set (get-vertices g))])
-  (sequence-fold max -inf.0 
-                 (sequence-map (curry shortest-from g vertices) 
-                               (in-vertices g))))
+(define (find-path g)
+  (let* ([vertices (list->set (get-vertices g))])
+    (sequence-fold max -inf.0 
+                   (sequence-map (λ (v) (shortest-from g vertices v v)) 
+                                 (in-vertices g)))))
+
+; (find-path (read-edges))
 
 ; part 1 just uses 'min' and +inf.0 instead of 'max' and -inf.0
---
+++ 2015/013.rkt
@@ -0,0 +1,25 @@
+#lang racket
+
+(require graph
+         "009.rkt")
+
+(define (parse-row s)
+  (let ([data (string-split s)])
+    (values (set (first data) (string-trim (last data) #px"\\."))
+            ((if (string=? (third data) "gain") + -) 
+             (string->number (fourth data))))))
+
+(define (load-edges)
+  (for/fold ([h (hash)])
+            ([edge (in-lines)])
+    (let-values ([(k val-inc) (parse-row edge)])
+      (hash-set h k (+ (hash-ref h k 0) val-inc)))))
+
+(define (build-graph edge-hash)
+  (weighted-graph/undirected
+    (for/list ([e (in-hash-pairs edge-hash)])
+      (cons (cdr e) (set->list (car e))))))
+
+(find-path
+  (build-graph 
+    (load-edges)))
