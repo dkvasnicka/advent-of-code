962c53c542ce8bd152f7792a5b40b98d972f31ab
dkvasnicka <daniel.kvasnicka.jr@gmail.com>
2016-12-08 14:58:58 +0100

    day 1, part 1 - cleanup

--- 2016/01.rkt
+++ 2016/01.rkt
@@ -1,35 +1,32 @@
 #lang rackjure
 
 (require srfi/13
-         math/array
-         lens
-         lens/data/hash)
+         math/array)
 
 (current-curly-dict hash)
-(struct cmd (adjustment result-dir) #:transparent)
+(struct rule (adjustment result-dir) #:transparent)
 
 (define traversal-rules
-  {'up    {"L" (cmd (array #[-1 0]) 'left) 
-           "R" (cmd (array #[1 0]) 'right)}
-   'right {"L" (cmd (array #[0 1]) 'up) 
-           "R" (cmd (array #[0 -1]) 'down)}
-   'down  {"L" (cmd (array #[1 0]) 'right) 
-           "R" (cmd (array #[-1 0]) 'left)}
-   'left  {"L" (cmd (array #[0 -1]) 'down) 
-           "R" (cmd (array #[0 1]) 'up)}})
+  {'up    {"L" (rule (array #[-1 0]) 'left) 
+           "R" (rule (array #[1 0]) 'right)}
+   'right {"L" (rule (array #[0 1]) 'up) 
+           "R" (rule (array #[0 -1]) 'down)}
+   'down  {"L" (rule (array #[1 0]) 'right) 
+           "R" (rule (array #[-1 0]) 'left)}
+   'left  {"L" (rule (array #[0 -1]) 'down) 
+           "R" (rule (array #[0 1]) 'up)}})
+
+(define (new-position pos adj mult)
+  (array+ pos (array-scale adj mult)))
 
 (define (walk-the-walk)
   (for/fold ([pos (array #[0 0])]
              [dir 'up])
             ([cmd (sequence-map symbol->string (in-port))])
-    (let ([c (lens-view 
-               (hash-ref-nested-lens dir (string-take cmd 1))
-               traversal-rules)])
-      (values
-        (array+ pos
-                (array-scale (cmd-adjustment c)
-                             (string->number (string-drop cmd 1))))
-        (cmd-result-dir c)))))
+    (let* ([c ((string-take cmd 1) (dir traversal-rules))]
+           [newpos (new-position pos (rule-adjustment c) (string->number 
+                                                           (string-drop cmd 1)))])
+      (values newpos (rule-result-dir c)))))
 
 (let-values ([(dest _) (walk-the-walk)])
   (array-all-sum (array-abs dest)))
