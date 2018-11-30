2a4f17ff6a2914c5d543e164a0454cf8086df89d
Daniel Kvasnicka <dkvasnicka@wiser.com>
2018-11-30 23:10:52 +0100

    2017, day 1 - flexing my muscles for 2018 edition

--- 2017/01.rkt
+++ 2017/01.rkt
@@ -1,3 +1,22 @@
 #lang racket
 
-(displayln (read))
+(define (char->number c)
+  (if (char-numeric? c)
+      (string->number (string c))
+      c))
+
+(define (same? x y)
+  (if (= x y) x 0))
+
+(define (process last-num nums sum head)
+  (if (equal? '(#\newline) nums)
+      (+ sum (same? last-num head))
+      (process
+        (car nums)
+        (cdr nums)
+        (+ sum (same? last-num (car nums)))
+        head)))
+
+(let* ([data (sequence->list (sequence-map char->number (in-port read-char)))]
+       [head (car data)])
+  (process head (cdr data) 0 head))
