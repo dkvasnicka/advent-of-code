1d743c2866b11f64a86a4eb42a233c36888ff5c9
Daniel Kvasnicka <dkvasnicka@wiser.com>
2018-12-01 21:32:51 +0100

    2018, day 1, part 2

--- 2018/01.rkt
+++ 2018/01.rkt
@@ -1,4 +1,14 @@
 #lang racket
 
-(for/sum ([n (in-lines)])
-  (string->number n))
+(require math/base
+         data/collection)
+
+(define data (for/list ([n (in-lines)]) (string->number n)))
+
+(displayln (sum data))
+
+(let ([register (mutable-seteq)]
+      [reductions (foldl/steps + 0 (cycle data))])
+  (for/last ([isum reductions] #:final (set-member? register isum))
+    (set-add! register isum)
+    isum))
