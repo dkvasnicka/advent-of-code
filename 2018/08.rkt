de1fe22baf5f9e0441abf667f883de58bdc8dcec
Daniel Kvasnicka <dkvasnicka@wiser.com>
2018-12-14 21:42:26 +0100

    2018, day 8, part 2

--- 2018/08.rkt
+++ 2018/08.rkt
@@ -1,9 +1,31 @@
 #lang racket
 
-(let loop ()
-  (let ([children (read)])
-    (if (eof-object? children)
-        0
-        (let ([metadata (read)])
-          (+ (for/sum ([_ (in-range children)]) (loop))
-             (for/sum ([_ (in-range metadata)]) (read)))))))
+(require (only-in srfi/43 vector-empty?)
+         math/base)
+
+; Part 1
+(with-input-from-file "08.txt"
+  (λ ()
+     (let loop ()
+       (let ([children (read)])
+         (if (eof-object? children)
+             0
+             (let ([metadata (read)])
+               (+ (for/sum ([_ (in-range children)]) (loop))
+                  (for/sum ([_ (in-range metadata)]) (read)))))))))
+
+; Part 2
+(with-input-from-file "08.txt"
+  (λ ()
+     (let loop ()
+       (let ([children (read)])
+         (if (eof-object? children)
+             0
+             (let* ([metadata (read)]
+                    [children-vals (for/vector ([_ (in-range children)]) (loop))]
+                    [children-count (vector-length children-vals)]
+                    [idxs (for/list ([_ (in-range metadata)]) (read))])
+              (if (vector-empty? children-vals)
+                  (sum idxs)
+                  (for/sum ([i idxs] #:when (and (>= i 1) (<= i children-count)))
+                    (vector-ref children-vals (sub1 i))))))))))
