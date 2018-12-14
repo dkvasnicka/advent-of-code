5e54ef5f0d39680af0363e76a169ee2d993985b2
Daniel Kvasnicka <dkvasnicka@wiser.com>
2018-12-14 14:23:18 +0100

    2018, day 8, part 1 in Typed Racket (slower than untyped???)

---
+++ 08_part1_typed.rkt
@@ -0,0 +1,12 @@
+#lang typed/racket/base
+
+(require/typed racket/base [read (-> Natural)])
+
+(time
+  (let loop : Natural ()
+   (let ([children (read)])
+     (if (eof-object? children)
+         0
+         (let ([metadata (read)])
+           (+ (for/sum : Natural ([_ (in-range children)]) (loop))
+              (for/sum : Natural ([_ (in-range metadata)]) (read))))))))
