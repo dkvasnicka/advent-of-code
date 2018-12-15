b1dd4579b6fe87fe34633dd5633b385e0ca930a3
Daniel Kvasnicka <dkvasnicka@wiser.com>
2018-12-15 11:09:11 +0100

    2018, day 8, part 1 in Typed Racket - update

--- 2018/08_part1_typed.rkt
+++ 2018/08_part1_typed.rkt
@@ -3,6 +3,9 @@
 (require typed/racket/unsafe)
 (unsafe-require/typed racket/base [read (-> Natural)])
 
+; Slower than the untyped version, probably because of IO
+; Discussed at https://www.reddit.com/r/Racket/comments/a64tys/why_is_this_typed_program_slower_than_its_untyped/
+
 (time
   (let loop : Natural ()
    (let ([children (read)])
