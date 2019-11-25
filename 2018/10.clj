8111d8d68e98eaf2dad2ac97d3419b682ad7d9d3
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-11-25 10:40:20 -0800

    2018 WIPs

---
+++ 2018/10.clj
@@ -0,0 +1,1 @@
+; https://nbviewer.jupyter.org/github/MageJohn/Advent-of-Code-2018/blob/master/Day%2010%3A%20The%20Stars%20Align/Day%2010%20Solutions.ipynb
---
+++ 2018/11.clj
@@ -0,0 +1,2 @@
+; https://en.wikipedia.org/wiki/Maximum_subarray_problem
+; https://www.geeksforgeeks.org/maximum-sum-rectangle-in-a-2d-matrix-dp-27/
