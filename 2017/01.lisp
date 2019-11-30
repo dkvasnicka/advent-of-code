a53a2eb0a6d7ce826cb2cabf49e08497ef42b5f1
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-11-29 17:26:28 -0800

    2017, day 1, part 1 in Common Lisp (prep for 2019 edition)

---
+++ 2017/01.lisp
@@ -0,0 +1,23 @@
+(defun same? (x y)
+  (if (= x y) x 0))
+
+(defun process (last-num nums sum head)
+  (if (null nums)
+      (+ sum (same? last-num head))
+      (process (car nums)
+               (cdr nums)
+               (+ sum (same? last-num (car nums)))
+               head)))
+
+(defun main ()
+  (princ
+    (let* ((data (for:for ((c over *standard-input*)
+                           (num = (digit-char-p c))
+                           (chars when num collect num))))
+           (head (car data)))
+      (process head (cdr data) 0 head))))
+
+; (series:collect
+; (series:choose-if #'alphanumericp
+; (series:scan-stream *standard-input*
+; #'read-char)))
