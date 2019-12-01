23c91e1f7cb193c356e8301e60b8af267e8deaf8
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-01 10:24:48 -0800

    2019, day 1 -- using floor with a divisor instead of ffloor; cleanup

--- 2019/01.lisp
+++ 2019/01.lisp
@@ -1,8 +1,12 @@
+(defpackage #:aoc2019d01
+  (:use :cl :series))
+
+(in-package #:aoc2019d01)
+
 (defun main ()
-  (princ
-    (series:collect-sum
-      (series:map-fn 'number
-                     (lambda (mass)
-                       (- (ffloor (/ mass 3)) 2))
-                     (series:scan-stream *standard-input* #'read))))
-  (quit))
+  (collect-sum
+    (map-fn 'number
+            (lambda (mass) (- (floor mass 3) 2))
+            (scan-stream *standard-input* #'read))))
+
+(princ (main))
