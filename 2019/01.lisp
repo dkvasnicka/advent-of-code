80dda013e52415d94417018dd0a1def9c9f865ae
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-01 16:37:33 -0800

    2019, day 1, part 2

--- 2019/01.lisp
+++ 2019/01.lisp
@@ -3,10 +3,16 @@
 
 (in-package #:aoc2019d01)
 
+(defun fuel-needed (mass)
+  (let ((initial-fuel (- (floor mass 3) 2)))
+    (if (<= initial-fuel 0)
+        0
+        (+ initial-fuel (fuel-needed initial-fuel)))))
+
 (defun main ()
   (collect-sum
     (map-fn 'number
-            (lambda (mass) (- (floor mass 3) 2))
+            #'fuel-needed
             (scan-stream *standard-input* #'read))))
 
 (princ (main))
