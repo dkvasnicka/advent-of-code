76894aefe6516b40a51fb95254abbb6007364a8b
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-02 21:23:24 -0800

    2019, day 2, part 1

---
+++ 2019/02.lisp
@@ -0,0 +1,42 @@
+(require "taps")
+(require "str")
+(require "random-access-lists")
+(defpackage #:aoc2019d02
+  (:use :cl :series :taps :alexandria :random-access-lists))
+(in-package #:aoc2019d02)
+
+(defparameter *suppress-series-warnings* t)
+
+(defvar *gravity-assist-program*
+  (ra-reverse
+    (collect-fn (type-of (ra-list)) #'ra-list (lambda (xs x) (ra-cons x xs))
+                (map-fn 'integer #'parse-integer
+                        (choose-if (compose #'not #'str:blank?)
+                                   (tap :words *standard-input*
+                                        :word-break-characters '(#\,)))))))
+
+(defmacro offset-pointer-val (offset)
+  `(ra-list-ref prg (ra-list-ref prg (+ idx ,offset))))
+
+(defun exec-instruction (code idx prg)
+  (ra-list-set prg
+               (ra-list-ref prg (+ idx 3))
+               (funcall
+                 (case code
+                   (1 #'+)
+                   (2 #'*))
+                 (offset-pointer-val 1)
+                 (offset-pointer-val 2))))
+
+(defun instruction-cycle (idx prg)
+  (let ((code (ra-list-ref prg idx)))
+    (if (= 99 code)
+        prg
+        (instruction-cycle (+ idx 4)
+                           (exec-instruction code idx prg)))))
+
+(defun main ()
+  (ra-car
+    (instruction-cycle 0 *gravity-assist-program*)))
+
+(princ (main))
