aba042c6dfdb4c933fa420f670c56552ba05756a
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-08 19:40:05 -0800

    2019, day 3, part 1

--- 2019/03.lisp
+++ 2019/03.lisp
@@ -23,12 +23,16 @@
     ('D (pt :x (x current-end)
             :y (- (y current-end) (cdr instruction))))))
 
+(defvar *origin* (make-instance 'geometry:point))
+
 (defun build-segment (segs instruction)
-  (let ((current-end (end (car segs))))
-    (cons (make-instance 'geometry:line-segment
-                         :start current-end
-                         :end (next-end current-end instruction))
-          segs)))
+  (let* ((current-end (end (car segs)))
+         (built (make-instance 'geometry:line-segment
+                               :start current-end
+                               :end (next-end current-end instruction))))
+    (if (geometry:point-equal-p current-end *origin*)
+        (list built)
+        (cons built segs))))
 
 (defun read-wirespec (line-string)
   (collect-fn 'list (lambda () (list (make-instance 'geometry:line-segment)))
@@ -39,5 +43,17 @@
 (defvar *wire1* (read-wirespec (read-line)))
 (defvar *wire2* (read-wirespec (read-line)))
 
+(defun manhattan-distance (p1 p2)
+  (+ (abs (- (x p1) (x p2)))
+     (abs (- (y p1) (y p2)))))
+
 (defun main ()
-  (princ *wire1*)) ; TODO for:for
+  (princ
+    (for:for ((segment :in *wire2*)
+              (min-dist = (for:for ((l1-segment :in *wire1*)
+                                    (xsection = (geometry:line-segments-intersection-point
+                                                  segment l1-segment
+                                                  :exclude-endpoints t))
+                                    (md :when xsection :minimize
+                                        (manhattan-distance *origin* xsection)))))
+              (md :when min-dist :minimize min-dist)))))
