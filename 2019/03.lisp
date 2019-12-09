1cf485301098617add11d8f978a27c1e06d21d33
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-08 20:17:55 -0800

    2019, day 3 - using iterate instead of for:for

--- 2019/03.lisp
+++ 2019/03.lisp
@@ -1,8 +1,7 @@
 (require "taps")
 (require "str")
 (require "cl-geometry")
-(defpackage #:aoc2019d03
-  (:use :cl :cl-geometry :series))
+(defpackage #:aoc2019d03 (:use :cl :cl-geometry :iterate))
 (in-package #:aoc2019d03)
 (defparameter *suppress-series-warnings* t)
 
@@ -35,25 +34,26 @@
         (cons built segs))))
 
 (defun read-wirespec (line-string)
-  (collect-fn 'list (lambda () (list (make-instance 'geometry:line-segment)))
-              #'build-segment
-              (map-fn 'list #'parse-instruction
-                      (taps:tap :words line-string))))
+  (series:collect-fn 'list
+                     (lambda () (list (make-instance 'geometry:line-segment)))
+                     #'build-segment
+                     (series:map-fn 'list #'parse-instruction
+                                    (taps:tap :words line-string))))
 
 (defvar *wire1* (read-wirespec (read-line)))
 (defvar *wire2* (read-wirespec (read-line)))
 
 (defun manhattan-distance (p1 p2)
-  (+ (abs (- (x p1) (x p2)))
-     (abs (- (y p1) (y p2)))))
+  (if (null p2)
+      sb-ext:double-float-positive-infinity
+      (+ (abs (- (x p1) (x p2)))
+         (abs (- (y p1) (y p2))))))
 
 (defun main ()
   (princ
-    (for:for ((segment :in *wire2*)
-              (min-dist = (for:for ((l1-segment :in *wire1*)
-                                    (xsection = (geometry:line-segments-intersection-point
-                                                  segment l1-segment
-                                                  :exclude-endpoints t))
-                                    (md :when xsection :minimize
-                                        (manhattan-distance *origin* xsection)))))
-              (md :when min-dist :minimize min-dist)))))
+    (iter outer (for segment in *wire2*)
+          (iter (for l1-segment in *wire1*)
+                (in outer (minimize (manhattan-distance
+                                      *origin*
+                                      (geometry:line-segments-intersection-point
+                                        segment l1-segment :exclude-endpoints t))))))))
