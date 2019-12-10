665c2f8a1b84244498230e0906b3a4807e06c092
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-09 22:17:26 -0800

    2019, day 3 - cleanup

--- 2019/03.lisp
+++ 2019/03.lisp
@@ -3,13 +3,13 @@
 (require "cl-geometry")
 (defpackage #:aoc2019d03 (:use :cl :cl-geometry :iterate :alexandria))
 (in-package #:aoc2019d03)
-(defparameter *suppress-series-warnings* t)
+(defparameter series:*suppress-series-warnings* t)
 
 (defun parse-instruction (instr)
   (cons (intern (str:s-first instr))
         (parse-integer (str:s-rest instr))))
 
-(serapeum:defalias pt (serapeum:partial #'make-instance 'geometry:point))
+(serapeum:defalias pt (serapeum:partial #'make-instance 'point))
 
 (defun next-end (current-end instruction)
   (ecase (car instruction)
@@ -22,33 +22,32 @@
     ('D (pt :x (x current-end)
             :y (- (y current-end) (cdr instruction))))))
 
-(defvar *origin* (make-instance 'geometry:point))
+(defvar *origin* (make-instance 'point))
 
 (defun build-segment (segs instruction)
   (let* ((current-end (end (last-elt segs)))
-         (built (make-instance 'geometry:line-segment
+         (built (make-instance 'line-segment
                                :start current-end
                                :end (next-end current-end instruction))))
-    (if (geometry:point-equal-p current-end *origin*)
-        (serapeum:vect built)
-        (serapeum:vector-conc-extend segs (vector built)))))
+    (serapeum:vector-conc-extend segs (vector built))))
 
 (defun read-wirespec (line-string)
-  (series:collect-fn 'vector
-                     (lambda () (vector (make-instance 'geometry:line-segment)))
-                     #'build-segment
-                     (series:map-fn 'list #'parse-instruction
-                                    (taps:tap :words line-string))))
+  (serapeum:drop 1
+    (series:collect-fn 'vector
+                       (lambda () (serapeum:vect (make-instance 'line-segment)))
+                       #'build-segment
+                       (series:map-fn 'list #'parse-instruction
+                                      (taps:tap :words line-string)))))
 
 (defun manhattan-distance (p1 p2)
   (+ (abs (- (x p1) (x p2)))
      (abs (- (y p1) (y p2)))))
 
 (defun xsection-pt (s1 s2)
-  (geometry:line-segments-intersection-point s1 s2 :exclude-endpoints t))
+  (line-segments-intersection-point s1 s2 :exclude-endpoints t))
 
 (defun dist (p1 p2)
-  (geometry:distance (x p1) (y p1) (x p2) (y p2)))
+  (distance (x p1) (y p1) (x p2) (y p2)))
 
 ; Most naive solutions are going point by point. This one uses trivial 2D
 ; geometry to find the solution to both parts of the problem in one go.
@@ -57,14 +56,14 @@
         (wire2 (read-wirespec (read-line))))
     (princ
       (iter outer (for segment in-vector wire2)
-            (sum (geometry:line-segment-length segment) into travelled1)
+            (sum (line-segment-length segment) into travelled1)
             (iter (for l1-segment in-vector wire1)
-                  (sum (geometry:line-segment-length l1-segment) into travelled2)
+                  (sum (line-segment-length l1-segment) into travelled2)
                   (in outer
                       (when-let ((xsection (xsection-pt segment l1-segment)))
-                                (minimizing (manhattan-distance *origin* xsection) into d)
-                                (minimizing (- (+ travelled1 travelled2)
-                                               (dist (end segment) xsection)
-                                               (dist (end l1-segment) xsection))
-                                            into min-travelled))
+                        (minimizing (manhattan-distance *origin* xsection) into d)
+                        (minimizing (- (+ travelled1 travelled2)
+                                       (dist (end segment) xsection)
+                                       (dist (end l1-segment) xsection))
+                                    into min-travelled))
                       (finally (return-from outer (list d min-travelled)))))))))
