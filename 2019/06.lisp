7aca9b975e90b61a4951441abb6da72360e9f719
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-14 22:41:17 -0800

    2019, day 6, part 2

--- 2019/06.lisp
+++ 2019/06.lisp
@@ -12,19 +12,29 @@
     (add-edge g a b))
   g)
 
-(defun sum-orbits (g &optional (n "COM") (steps 0))
-  (let ((neighbors (outbound-neighbors g n)))
+(defun sum-orbits (g &optional (n "COM") (prev -1) (steps 0))
+  (let ((nbrs (remove prev (neighbors g n))))
     (+ steps
-       (if (null neighbors)
+       (if (null nbrs)
            0
-           (iter (for nbr in neighbors)
-                 (sum (sum-orbits g nbr (1+ steps))))))))
+           (iter (for nbr in nbrs)
+                 (sum (sum-orbits g nbr n (1+ steps))))))))
 
-(defun main ()
-  (princ
-    (iter (for l in-stream *standard-input* :using #'read-line)
+(defun read-graph ()
+  (iter (for l in-stream *standard-input* :using #'read-line)
           (accumulate (split-sequence #\) l) by #'add-nodes-and-edge
-                      initial-value (make-graph :directed? t)
+                      initial-value (make-graph)
                       into g)
           (finally
-            (return (sum-orbits g))))))
+            (return g))))
+
+(defun path-to-santa (g)
+  (- (length (find-shortest-path g "YOU" "SAN"))
+     2))
+
+(defun main ()
+  (let ((g (read-graph)))
+    ; Part 1
+    (format t "~a~%" (sum-orbits g (lookup-node g "COM")))
+    ; Part 2
+    (princ (path-to-santa g))))
