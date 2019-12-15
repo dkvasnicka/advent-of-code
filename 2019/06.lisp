caa6db5f9d4969dccca17d70fa113489e43dbd91
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-14 22:16:19 -0800

    Code style

--- 2019/06.lisp
+++ 2019/06.lisp
@@ -1,29 +1,30 @@
 (require "graph-utils")
 (defpackage #:aoc2019d06
-  (:use :cl :iterate :alexandria :serapeum :rove)
-  (:shadowing-import-from :iterate collecting summing sum in))
+  (:use :cl :iterate :alexandria :serapeum :rove :graph-utils)
+  (:shadowing-import-from :iterate collecting summing sum in)
+  (:shadowing-import-from :graph-utils select))
 (in-package #:aoc2019d06)
 
-(defun add-node (nodes g)
+(defun add-nodes-and-edge (nodes g)
   (destructuring-bind (a b) nodes
-    (graph-utils:add-node g a)
-    (graph-utils:add-node g b)
-    (graph-utils:add-edge g a b))
+    (add-node g a)
+    (add-node g b)
+    (add-edge g a b))
   g)
 
 (defun sum-orbits (g &optional (n "COM") (steps 0))
-  (let ((neighbors (graph-utils:outbound-neighbors g n)))
-    (if (null neighbors)
-        steps
-        (+ steps
+  (let ((neighbors (outbound-neighbors g n)))
+    (+ steps
+       (if (null neighbors)
+           0
            (iter (for nbr in neighbors)
                  (sum (sum-orbits g nbr (1+ steps))))))))
 
 (defun main ()
   (princ
     (iter (for l in-stream *standard-input* :using #'read-line)
-          (accumulate (split-sequence #\) l) by #'add-node
-                      initial-value (graph-utils:make-graph :directed? t)
+          (accumulate (split-sequence #\) l) by #'add-nodes-and-edge
+                      initial-value (make-graph :directed? t)
                       into g)
           (finally
             (return (sum-orbits g))))))
