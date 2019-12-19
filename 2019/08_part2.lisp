dbf5d9b00763fc9aadf9117f5aa5ca5d9bc90175
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-18 23:50:43 -0800

    2019, day 8, part 2 - reading layers into 2D arrays

---
+++ 2019/08_part2.lisp
@@ -0,0 +1,26 @@
+(require "array-operations")
+(defpackage #:aoc2019d08p2
+  (:use :cl :iterate :alexandria :serapeum :parachute)
+  (:shadowing-import-from :iterate collecting until finish collect summing sum in)
+  (:shadowing-import-from :parachute of-type featurep true))
+(in-package #:aoc2019d08p2)
+
+(defun read-layers (is w h)
+  (cons (aops:generate (lambda () (digit-char-p (read-char is)))
+                       (list h w))
+        (let ((next-char (peek-char nil is nil)))
+          (if (or (null next-char) (whitespacep next-char))
+              '()
+              (read-layers is w h)))))
+
+(defun main ()
+  (princ
+    (read-layers *standard-input* 25 6)))
+
+(define-test "read layers into a list of 2D arrays"
+  (is equalp
+      (list #2A((1 1 2) (2 0 1)) #2A((0 0 2) (1 2 1)))
+      (with-input-from-string (s "112201002121")
+        (read-layers s 3 2))))
+
+(test *package*)
