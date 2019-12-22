fd17bc8e326f954d285577feb7a2162cd1982ea4
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-22 13:05:46 -0800

    2019, day 10, part 1

--- 2019/10.lisp
+++ 2019/10.lisp
@@ -7,22 +7,33 @@
 
 (defstruct pt x y)
 
-(defun add-point (coords pts)
-  (destructuring-bind (y x) coords
-    (fset:with pts (make-pt :x x :y y))))
-
 (defun read-asteroids (s size)
   (iter (for idx from 0 below (expt size 2))
-        (when (equal (read-char s) #\#)
-              (accumulate (multiple-value-list (floor idx size)) :by #'add-point
-                          :initial-value (fset:empty-set)))))
+        (when (eq (read-char s) #\#)
+              (collect (mvlet ((y x (floor idx size)))
+                         (make-pt :x x :y y))))))
+
+(defun angle (s a)
+  (if (equalp s a)
+      nil
+      (atan (- (pt-y a) (pt-y s))
+            (- (pt-x a) (pt-x s)))))
+
+(defun count-visible-asteroids (station asteroids)
+  (fset:size
+    (gmap:gmap (:set :filterp #'true)
+               (partial #'angle station)
+               (:list asteroids))))
 
 (defun main ()
-  (princ (read-asteroids *standard-input* 27)))
+  (princ
+    (let ((asteroids (read-asteroids *standard-input* 27)))
+      (iter (for a in asteroids)
+            (maximize (count-visible-asteroids a asteroids))))))
 
 (define-test "create a set of points"
   (is equalp
-      (fset:set (make-pt :x 0 :y 1))
+      (list (make-pt :x 0 :y 1))
       (with-input-from-string (s "..#.")
         (read-asteroids s 2))))
 
