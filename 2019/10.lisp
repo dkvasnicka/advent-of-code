bbe4d2678a3d4b0a523d69559ec79c15901efbfd
Daniel Kvasnicka <dkvasnicka@wiser.com>
2019-12-28 16:42:59 -0800

    2019, day 10 - switching to clockwise 360 deg angle system in prep for part 2

--- 2019/10.lisp
+++ 2019/10.lisp
@@ -16,18 +16,25 @@
 (defun angle (s a)
   (if (equalp s a)
       nil
-      (atan (- (pt-y a) (pt-y s))
-            (- (pt-x a) (pt-x s)))))
+      (let ((degrees (round-to
+                       (* (/ 180 pi) (atan (- (pt-x a) (pt-x s))
+                                           (- (pt-y a) (pt-y s))))
+                       0.00001)))
+        (if (minusp degrees)
+            (+ degrees 360)
+            degrees))))
 
 (defun count-visible-asteroids (station asteroids)
   (iter (for a in asteroids)
         (when-let (alpha (angle station a))
+          ; TODO: RB-tree????
           (accumulate alpha :by (flip #'hs-ninsert)
                       :initial-value (make-hash-set) :into angles))
         (finally (return (hs-count angles)))))
 
 (defun main ()
   (princ
+    ; TODO: find maximizing; let & return a, count of angles, angles
     (let ((asteroids (read-asteroids *standard-input* 27)))
       (iter (for a in asteroids)
             (maximize (count-visible-asteroids a asteroids))))))
@@ -37,5 +44,17 @@
       (list (make-pt :x 0 :y 1))
       (with-input-from-string (s "..#.")
         (read-asteroids s 2))))
+
+(define-test "compute angle in 0-360 deg scale"
+  (is = 45 (angle (make-pt :x 0 :y 0)
+                  (make-pt :x 1 :y 1)))
+  (is = 135 (angle (make-pt :x 0 :y 0)
+                   (make-pt :x 1 :y -1)))
+  (is = 180 (angle (make-pt :x 0 :y 0)
+                   (make-pt :x 0 :y -1)))
+  (is = 225 (angle (make-pt :x 0 :y 0)
+                   (make-pt :x -1 :y -1)))
+  (is = 315 (angle (make-pt :x 0 :y 0)
+                   (make-pt :x -1 :y 1))))
 
 (test *package*)
