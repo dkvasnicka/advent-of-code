a2cb1db094e279a5f72374be4c6621a5693b6555
Daniel Kvasnicka <dkvasnicka@wiser.com>
2020-01-05 00:41:14 -0800

    2019, day 12, part 2

--- 2019/12.lisp
+++ 2019/12.lisp
@@ -1,6 +1,7 @@
 (require "array-operations")
+(require "losh")
 (defpackage #:aoc2019d12
-  (:use :iterate :cl21 :cl21.lazy :rutils.bind)
+  (:use :cl :iterate :rutils.bind :losh.iterate)
   (:shadowing-import-from :iterate while until)
   (:shadowing-import-from :rutils.bind with))
 (in-package #:aoc2019d12)
@@ -42,20 +43,30 @@
 (defun apply-velocities (pos vel)
   (aops:each #'+ pos vel))
 
-(defun trajectories-seq (pos vel)
-  (lazy-sequence
-    (let* ((new-vel (compute-velocities pos vel))
-           (new-pos (apply-velocities pos new-vel)))
-      (cons (list new-pos new-vel)
-            (trajectories-seq new-pos new-vel)))))
+(defun trajectories (pos vel)
+  (let* ((new-vel (compute-velocities pos vel))
+         (new-pos (apply-velocities pos new-vel)))
+    (cons new-pos new-vel)))
 
 (defun energy (a)
   (aops:each-index i (aops:sum-index j (abs (aref a i j)))))
 
+(defun zeroed-axis? (ary axis)
+  (aops:reduce-index #'rutils.misc:and2 i (zerop (aref ary i axis))))
+
 (defun main ()
   (princ
-    (with ((trajectories (trajectories-seq *moons-positions* *moons-velocities*))
-           ((pos1000 vel1000) (elt trajectories 999))
-           (potentials (energy pos1000))
-           (kinetics (energy vel1000)))
-          (reduce #'+ (map #'* potentials kinetics)))))
+    (time
+      (iter (for s from 1)
+            (for (p . v) first (trajectories *moons-positions* *moons-velocities*)
+                 then (trajectories p v))
+            (when (= s 1000)
+              (for nrg = (let ((potentials (energy p))
+                               (kinetics (energy v)))
+                           (reduce #'+ (map 'list #'* potentials kinetics)))))
+            ; https://www.reddit.com/r/adventofcode/comments/e9nqpq/day_12_part_2_2x_faster_solution/
+            (finding-first s such-that (zeroed-axis? v 0) into x)
+            (finding-first s such-that (zeroed-axis? v 1) into y)
+            (finding-first s such-that (zeroed-axis? v 2) into z)
+            (until (and x y z (>= s 1000)))
+            (returning (cons nrg (* 2 (lcm x y z))))))))
