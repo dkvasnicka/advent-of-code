67f29144175adfa523c819991e23bd2ad13bd3f9
Daniel Kvasnicka <dkvasnicka@wiser.com>
2020-01-19 21:13:35 -0800

    2016, day 4, part 1

---
+++ 2016/04.lisp
@@ -0,0 +1,34 @@
+(load "package.lisp")
+(require "cl-ppcre")
+(in-package :aoc2016)
+
+(defun most-common-chars (hist)
+  (map 'list #'car
+       (sort (hash-table-alist hist)
+             (lambda (l r) (or (> (cdr l) (cdr r))
+                               (and (= (cdr l) (cdr r))
+                                    (char< (car l) (car r))))))))
+
+(defun real-room? (sector-name checksum)
+  (let ((sector-name-hist (iter (for ch in-string sector-name)
+                                (when (alphanumericp ch)
+                                  (collect-frequencies ch)))))
+    (starts-with-subseq checksum (most-common-chars sector-name-hist))))
+
+(defun main ()
+  (pr
+    (time
+      (iter (for l in-stream *standard-input* using #'read-line)
+            (cl-ppcre:register-groups-bind
+              (sector-name sector-id checksum)
+              ("((?:[a-z]+\\-)+)(\\d+)\\[([a-z]+)\\]" l)
+              (when (real-room? sector-name checksum)
+                (sum (parse-integer sector-id))))))))
+
+; Evaluation took:
+  ; 0.011 seconds of real time
+  ; 0.010700 seconds of total run time (0.010549 user, 0.000151 system)
+  ; 100.00% CPU
+  ; 12 lambdas converted
+  ; 30,039,906 processor cycles
+  ; 3,723,568 bytes consed
--- 2016/package.lisp
+++ 2016/package.lisp
@@ -3,10 +3,14 @@
 (require "rutils")
 (require "series")
 (require "losh")
+(require "serapeum")
 (defpackage :aoc2016
-  (:use :cl :iterate :rutils.bind :alexandria :series
+  (:use :cl :iterate :rutils.bind :serapeum :alexandria :series
    :LOSH.DEBUGGING :losh.iterate)
-  (:shadowing-import-from :iterate iterate collect while until)
+  (:shadowing-import-from :iterate iterate summing collecting collect
+   in while sum until)
+  (:shadowing-import-from :series scan)
+  (:shadowing-import-from :serapeum @ comment bits)
   (:shadowing-import-from :rutils.bind with)
   (:shadowing-import-from :losh collect-hash))
 (in-package :aoc2016)
