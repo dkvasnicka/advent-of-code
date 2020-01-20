f857dd272f2243c8d54bd8e98f51e581a25c9c30
Daniel Kvasnicka <dkvasnicka@wiser.com>
2020-01-20 00:24:37 -0800

    2016, day 4 - using persistent sorted list is ~ 10x slower

--- 2016/04.lisp
+++ 2016/04.lisp
@@ -2,12 +2,19 @@
 (require "cl-ppcre")
 (in-package :aoc2016)
 
+(defun comp-chars (l r)
+  (with (((l-ch l-cnt) l) ((r-ch r-cnt) r))
+    (or (> l-cnt r-cnt)
+        (and (= l-cnt r-cnt) (char< l-ch r-ch)))))
+
 (defun most-common-chars (hist)
-  (map 'list #'car
-       (sort (hash-table-alist hist)
-             (lambda (l r) (or (> (cdr l) (cdr r))
-                               (and (= (cdr l) (cdr r))
-                                    (char< (car l) (car r))))))))
+  (collect-elements
+    (iter (for (ch cnt) in-hashtable hist)
+          (accumulate (list ch cnt)
+                      :by (flip #'insert-new-item)
+                      :initial-value (make-instance 'sorted-list-container
+                                                    :sorter #'comp-chars)))
+    :transform #'car))
 
 (defun real-room? (sector-name checksum)
   (starts-with-subseq checksum (most-common-chars
@@ -25,9 +32,10 @@
                 (sum (parse-integer sector-id))))))))
 
 ; Evaluation took:
-  ; 0.011 seconds of real time
-  ; 0.010700 seconds of total run time (0.010549 user, 0.000151 system)
+  ; 0.099 seconds of real time
+  ; 0.098724 seconds of total run time (0.084180 user, 0.014544 system)
+  ; [ Run times consist of 0.067 seconds GC time, and 0.032 seconds non-GC time. ]
   ; 100.00% CPU
-  ; 12 lambdas converted
-  ; 30,039,906 processor cycles
-  ; 3,723,568 bytes consed
+  ; 112 lambdas converted
+  ; 277,395,235 processor cycles
+  ; 10,193,408 bytes consed
--- 2016/package.lisp
+++ 2016/package.lisp
@@ -4,13 +4,14 @@
 (require "series")
 (require "losh")
 (require "serapeum")
+(require "cl-containers")
 (defpackage :aoc2016
   (:use :cl :iterate :rutils.bind :serapeum :alexandria :series
-   :LOSH.DEBUGGING :losh.iterate)
+   :LOSH.DEBUGGING :losh.iterate :metabang.cl-containers)
   (:shadowing-import-from :iterate iterate summing collecting collect
-   in while sum until)
+   in while sum until finish)
   (:shadowing-import-from :series scan)
-  (:shadowing-import-from :serapeum @ comment bits)
+  (:shadowing-import-from :serapeum @ comment bits filter)
   (:shadowing-import-from :rutils.bind with)
   (:shadowing-import-from :losh collect-hash))
 (in-package :aoc2016)
