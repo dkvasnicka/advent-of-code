c35113231b6979064704725383401781e1e866ee
Daniel Kvasnicka <dkvasnicka@wiser.com>
2020-06-06 21:03:44 -0700

    2016, day 5, part 1 - raw version

--- 2016/05/build.gradle.kts
+++ 2016/05/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     kotlin("jvm") version "1.3.61"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
 }
 
 application {
@@ -8,6 +9,7 @@ application {
 }
 
 dependencies {
+    compile("commons-codec:commons-codec:1.14")
     compile(kotlin("stdlib"))
 }
 
--- 2016/05/src/main/kotlin/aoc/Day5.kt
+++ 2016/05/src/main/kotlin/aoc/Day5.kt
@@ -1,5 +1,18 @@
 package aoc
 
+import org.apache.commons.codec.digest.DigestUtils
+
 fun main(args: Array<String>) {
-    println("Hello, world!")
+    val input = "cxdnnyjw"
+
+    val result = (0..Int.MAX_VALUE).asSequence()
+        .map { input + it.toString() }
+        .map { it.toByteArray() }
+        .map { DigestUtils.md5Hex(it) }
+        .filter { it.startsWith("00000") }
+        .map { it.toByteArray()[5] }
+        .take(8)
+        .toList()
+
+        println(String(result.toByteArray()))
 }
