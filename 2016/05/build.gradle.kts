5955ad69aef9f1f90b18f50df6fbbf19b3fff854
Daniel Kvasnicka <dkvasnicka@wiser.com>
2020-06-06 17:32:38 -0700

    2016, day 5, Kotlin project setup

---
+++ 2016/05/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    application
+    kotlin("jvm") version "1.3.61"
+}
+
+application {
+    mainClassName = "aoc.Day5Kt"
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+}
+
+repositories {
+    jcenter()
+}
---
+++ 2016/05/src/main/kotlin/aoc/Day5.kt
@@ -0,0 +1,5 @@
+package aoc
+
+fun main(args: Array<String>) {
+    println("Hello, world!")
+}
