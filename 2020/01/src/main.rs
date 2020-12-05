0102c9bc187161e8d4d04662f4cb03d273dcedc3
Daniel Kvasnicka <dkvasnicka@wiser.com>
2020-12-05 16:45:06 +0100

    2020 day 1, part 2

--- 2020/01/src/main.rs
+++ 2020/01/src/main.rs
@@ -2,6 +2,21 @@ use std::collections::BTreeSet;
 use std::io;
 use std::io::prelude::*;
 
+fn decompose(tree: &BTreeSet<i32>, start: i32, level: i8) -> Option<i32> {
+    return tree.iter().find_map(|expense| {
+        let counterpart = start - expense;
+        if level == 0 {
+            if tree.contains(&counterpart) {
+                Some(expense * counterpart)
+            } else {
+                None
+            }
+        } else {
+            decompose(tree, counterpart, level - 1).map(|i| expense * i)
+        }
+    });
+}
+
 fn main() {
     let stdin = io::stdin();
     let nums = stdin
@@ -14,14 +29,9 @@ fn main() {
         tree.insert(n);
     }
 
-    let product = tree.iter().find_map(|expense| {
-        let counterpart = 2020 - expense;
-        if tree.contains(&counterpart) {
-            Some(expense * counterpart)
-        } else {
-            None
-        }
-    });
+    let product_p1 = decompose(&tree, 2020, 0);
+    println!("{}", product_p1.unwrap());
 
-    println!("{}", product.unwrap())
+    let product_p2 = decompose(&tree, 2020, 1);
+    println!("{}", product_p2.unwrap())
 }
