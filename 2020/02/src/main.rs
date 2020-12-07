37d32de501c6689d17506b6aa58de120541601a8
Daniel Kvasnicka <dkvasnicka@wiser.com>
2020-12-07 11:08:03 +0100

    2020 day 2, part 2

--- 2020/02/src/main.rs
+++ 2020/02/src/main.rs
@@ -13,23 +13,41 @@ lazy_static! {
 
 fn main() {
     let stdin = io::stdin();
-    let nums = stdin.lock().lines().map(|line| {
-        let line_str = line.unwrap();
-        let parsed = RE.captures(line_str.as_str()).unwrap();
-        (
-            parsed["lower"].parse::<u8>().unwrap(),
-            parsed["upper"].parse::<u8>().unwrap(),
-            parsed["letter"].to_string(),
-            parsed["password"].to_string(),
-        )
-    });
+    let nums = stdin
+        .lock()
+        .lines()
+        .map(|line| {
+            let line_str = line.unwrap();
+            let parsed = RE.captures(line_str.as_str()).unwrap();
+            (
+                parsed["lower"].parse::<usize>().unwrap(),
+                parsed["upper"].parse::<usize>().unwrap(),
+                parsed["letter"].to_string(),
+                parsed["password"].to_string(),
+            )
+        })
+        .collect::<Vec<(usize, usize, String, String)>>();
 
     let cnt = nums
+        .iter()
         .filter(|(lower, upper, letter, pwd)| {
-            let letter_count = pwd.matches(letter).count() as u8;
+            let letter_count = pwd.matches(letter).count();
             (*lower..upper + 1).contains(&letter_count)
         })
         .count();
 
-    println!("{}", cnt)
+    println!("{}", cnt);
+
+    let cnt2 = nums
+        .iter()
+        .filter(|(lower, upper, letter, pwd)| {
+            let idxs = pwd
+                .match_indices(letter)
+                .map(|p| p.0 + 1)
+                .collect::<Vec<usize>>();
+            idxs.contains(lower) ^ idxs.contains(upper)
+        })
+        .count();
+
+    println!("{}", cnt2)
 }
