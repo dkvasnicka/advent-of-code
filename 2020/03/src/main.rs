4cf31467a0746aea3e877bafcd5830ba679685a2
Daniel Kvasnicka <dkvasnicka@wiser.com>
2020-12-09 13:26:46 +0100

    2020 day 3 simpler counting algo

--- 2020/03/src/main.rs
+++ 2020/03/src/main.rs
@@ -3,22 +3,20 @@ use std::io::prelude::*;
 
 fn main() {
     let stdin = io::stdin();
-    let count = stdin
+    let count: u8 = stdin
         .lock()
         .lines()
         .skip(1)
-        .map(|line| line.unwrap().chars().collect::<Vec<char>>())
-        .fold((0, 0), |(pos, cnt), l| {
-            let new_pos = (pos + 3) % l.len();
-            (
-                new_pos,
-                cnt + match l[new_pos] {
-                    '#' => 1,
-                    '.' => 0,
-                    _ => panic!(),
-                },
-            )
-        });
+        .map(|line| line.unwrap())
+        .scan(0, |pos, l| {
+            *pos = (*pos + 3) % l.len();
+            Some(match l.get(*pos..*pos + 1).unwrap() {
+                "#" => 1,
+                "." => 0,
+                _ => panic!(),
+            })
+        })
+        .sum();
 
     println!("{:?}", count)
 }
