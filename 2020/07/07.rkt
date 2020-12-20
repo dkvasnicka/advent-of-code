c3ad84cb548e0334ddd4e05f5ddd4df9dcc40a7a
Daniel Kvasnicka <dkvasnicka@wiser.com>
2020-12-20 13:37:25 +0100

    2020 day 7 - tracking all bag content

--- 2020/07/07.rkt
+++ 2020/07/07.rkt
@@ -1,31 +1,46 @@
 #lang racket
 
 (require racklog
-         (only-in threading ~>))
+         (only-in threading ~>)
+         (only-in rackjure str))
 
-(struct bag (color content) #:transparent)
+(struct bag (color content cnt)
+  #:methods gen:equal+hash
+  [(define (equal-proc b1 b2 recursive-equal?)
+     (equal? (bag-color b1) (bag-color b2)))
+   (define (hash-proc bag __)
+     (equal-hash-code bag))
+   (define (hash2-proc bag __)
+     (equal-secondary-hash-code bag))]
+  #:methods gen:custom-write
+  [(define (write-proc b port mode)
+     (write-string (str "#<" (bag-color b) ">") port))])
 
 (define *head-regex* #px"^([\\w\\s]+) bags contain ((\\d+) ([\\w\\s]+)|no other) bag(s{0,1})")
 (define *other-regex* #px"^(\\d+) ([\\w\\s]+) bag(s{0,1})")
 
 (define (parse-bag line)
   (match-let* ([(list head other ...) (string-split line ",")]
-               [(pregexp *head-regex* (list _ color _ _ content _)) head])
+               [(pregexp *head-regex* (list _ color _ cnt content _)) head])
     (bag color
          (flatten
-           (cons (or content '())
+           (cons (if content (bag content '() cnt) '())
                  (for/list ([bagspec other])
-                   (third
-                     (regexp-match *other-regex* (string-trim bagspec)))))))))
+                   (bag
+                     (third
+                       (regexp-match *other-regex* (string-trim bagspec)))
+                     '()
+                     0))))
+         0)))
 
 (define %contains %empty-rel)
 
-(for ([b (for/stream ([l (in-lines)] #:unless (string-prefix? l "shiny gold"))
+(for ([b (for/stream ([l (in-lines)] #:unless (string-prefix? l "shiny gold")) ; Part 2 needs this
            (parse-bag l))]
       #:unless (null? (bag-content b)))
   (for ([in (bag-content b)])
     (%assert! %contains ()
-              [((bag-color b) in)])))
+              [(b in)])))
 
 (define %eventually-contains
   (%rel (x y z)
@@ -33,9 +48,22 @@
         [(x z) (%contains x y)
                (%eventually-contains y z)]))
 
-(let ([pt1 (%which (bags-with-gold-bags)
-                   (%let (a-bag x)
-                         (%set-of x (%eventually-contains x "shiny gold")
-                                  bags-with-gold-bags)))])
-  (displayln (~> pt1 first length sub1))
-  pt1)
+; Part 1
+(displayln
+  (~> (%which (bags-with-gold-bags)
+              (%let (a-bag x)
+                    (%set-of x (%eventually-contains x (bag "shiny gold" '() 0))
+                             bags-with-gold-bags)))
+      first
+      length
+      sub1))
+
+; Part 2
+; (displayln
+  ; (~> (%which (bags-in-my-gold-bag)
+              ; (%let (a-bag x)
+                    ; (%set-of x (%eventually-contains "shiny gold" x)
+                             ; bags-in-my-gold-bag)))
+      ; first
+      ; length
+      ; sub1))
