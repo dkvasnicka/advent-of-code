f5cfda8525768338dad7cc6b889735c8d2e8711e
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2021-12-02 14:13:53 +0100

    2021, day 1, part 2

--- 2021/01/src/main.rs
+++ 2021/01/src/main.rs
@@ -8,11 +8,17 @@ fn main() {
     let nums = stdin
         .lock()
         .lines()
-        .map(|line| line.unwrap().trim().parse::<i32>().unwrap());
+        .map(|line| line.unwrap().trim().parse::<i32>().unwrap())
+        .collect_vec();
+
+    let win_size = 3;
+    // let win_size = 1;
 
     println!(
-        "{}",
-        nums.tuple_windows()
+        "{:?}",
+        nums.windows(win_size)
+            .map(|w| w.iter().sum::<i32>())
+            .tuple_windows()
             .filter_map(|(l, r)| (r > l).then_some(1))
             .sum::<i32>()
     );
