9b5699d7667a0664d1299d0201b66d5e4b423c8c
Daniel Kvasnicka <daniel.kvasnicka@me.com>
2021-12-13 11:37:34 +0100

    2021 day 2, part 1

--- 2021/02/02.rkt
+++ 2021/02/02.rkt
@@ -1,22 +1,34 @@
-#lang typed/racket
+#lang racket
 
-(require/typed rackjure/str
-               [str (-> Any String)])
-(require relation/composition)
+(require relation/composition
+         racket/generic)
 
-(define-type Position (Tuple Number Number))
-(struct command ([dir : String] [step : Number])
-  #:type-name Command)
+; This solution is ridiculously chatty and would be a lot shorter if
+; `position` was just a simple list. However being inspired by Rust
+; I wanted to try out idiomatic struct-oriented programming approach
+; in Racket using generic interfaces.
+
+(define-generics encodable (encode encodable))
+
+(struct position (horizontal depth) #:transparent
+  #:methods gen:addable
+  [(define (add p1 p2)
+     (position (+ (position-horizontal p1) (position-horizontal p2))
+               (+ (position-depth p1) (position-depth p2))))]
+  #:methods gen:encodable
+  [(define (encode p) (* (position-horizontal p) (position-depth p)))])
+
+(struct command (dir step) #:transparent)
 
 (define (commands-seq)
-  (sequence-map
-    (compose (match-lambda [(list dir step)
-                            (command (str dir) (cast (string->number (str step)) Number))])
-             string-split)
-    (in-lines)))
+  (for/stream ([l (in-lines)])
+    (match (string-split l)
+      [(list dir step) (command dir (string->number step))])))
 
-(for/fold ([position : Position '(0 0)])
-  ([command (commands-seq)])
-  (map + position
-       (match command
-         [(Command "forward" x) (list x 0)])))
+(encode
+  (for/fold ([pos (position 0 0)]) ([cmd (commands-seq)])
+    (+ pos
+       (match cmd
+         [(command "forward" x) (position x 0)]
+         [(command "down" x) (position 0 x)]
+         [(command "up" x) (position 0 (- x))]))))
