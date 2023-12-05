9f28b1a67c6d81fdcfcd356d89583510baa56f03
Daniel Kvasnicka <daniel.kvasnicka@hey.com>
2023-12-05 15:33:04 +0100

    2023, day 1 part 1 - pretty

--- 2023/01/src/main.rs
+++ 2023/01/src/main.rs
@@ -6,21 +6,13 @@ use std::io::prelude::*;
 fn main() -> Result<()> {
     let stdin = io::stdin();
     let nums = stdin.lock().lines().filter_map(|line| {
-        let digits = line
-            .unwrap()
-            .trim()
-            .chars()
-            .filter(|c| c.is_digit(10))
-            .collect_vec();
+        let digits = line.ok()?.chars().filter(|c| c.is_digit(10)).collect_vec();
 
         digits.first().and_then(|d1| {
-            digits.last().map(|d2| {
-                vec![d1, d2]
-                    .iter()
-                    .map(|x| x.to_owned())
-                    .collect::<String>()
-                    .parse::<i32>()
-                    .unwrap()
+            digits.last().and_then(|d2| {
+                let mut s = String::from(*d1);
+                s.push(*d2);
+                s.parse::<i32>().ok()
             })
         })
     });
